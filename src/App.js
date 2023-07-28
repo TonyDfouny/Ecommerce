b4446eb669a929ac5fa49605b198c3abffe1247b
Tony Dfouny <tony_dfouny@hotmail.com>
2023-07-28 16:24:41 +0300

    last

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {Navbar} from "./components/navbar"
+import {Navbar} from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from './context/shop-context';
+
+
 function App() {
   return (
     <div className="App">
+      <ShopContextProvider>
       <Router>
         <Navbar/>
         <Routes>
-          <Route path="/"/>
-          <Route path="/cart"/>
+          <Route path="/" element={<Shop />}/>
+          <Route path="/cart" element={<Cart />}/>
         </Routes>
 
       </Router>
-
+      </ShopContextProvider>
     </div>
   );
 }
